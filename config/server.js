c7843410f5af73d76b87458fb07846708d35b902
bertyn <bertynboulikou@gmail.com>
2021-03-23 11:16:06 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54195be46fdf757b2d7958673a0cdf4a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8abe2682-63dd-4a5e-8f87-3f2855d4b2d1'
+};
